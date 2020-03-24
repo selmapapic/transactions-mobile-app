056b0500b5a287ff36c379bcf2ad19dae3ed0b0a
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-03-24 20:01:27 +0100

    Kreirana model klasa i izgled za element liste

---
+++ app/src/main/java/com/example/rma20celosmanovicselma04/TransactionsModel.java
@@ -0,0 +1,11 @@
+package com.example.rma20celosmanovicselma04;
+
+import java.util.ArrayList;
+
+public class TransactionsModel {
+    public static ArrayList<Transaction> transactions = new ArrayList<Transaction>() {
+        {
+            //napuniti
+        }
+    };
+}
