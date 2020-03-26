e10deabeb85f41228232ff5726cc632922f4d969
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-03-27 00:59:11 +0100

    Omoguceno otvaranje nove aktivnosti odabirom neke transakcije iz liste

--- app/src/main/java/com/example/rma20celosmanovicselma04/TransactionDetailActivity.java
+++ app/src/main/java/com/example/rma20celosmanovicselma04/TransactionDetailActivity.java
@@ -1,6 +1,14 @@
 package com.example.rma20celosmanovicselma04;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TransactionDetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_transactions);
+    }
 }
